21bf6b0c08543601b048f79c455e89754c57f3af
adarshvs1234 <va854173@gmail.com>
2024-11-29 11:17:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+
+function App() {
+  return (
+  <div id="root">
+    <div class="app">
+    <div class="app-body"> 
+
+    <div>
+   <button>Increment</button>
+   <label id="value">--</label>
+   <button>Decrement</button>
+
+  </div>
+  </div>
+  </div>
+  </div>
+
+  );
+}
+
+
+
+export default App;
+
+
+
